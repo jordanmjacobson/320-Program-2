124f988dccf1fa1ceaaeef3db46023d8880085c9
jjacob44 <jjacob44@binghamton.edu>
2020-05-13 15:05:37 -0500

    safety copy

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -19,6 +19,7 @@ public:
 };
 
 vector<Node> bits;
+vector<Node> bits_debug;
 
 /*void update_tree(int index){
   int num = 512;
@@ -86,11 +87,45 @@ result part6(int);
 }*/
 int free_spot(Line * arr){
   for(int i = 0; i<512;i++){
-    if (arr[i].flag() == "blank"){
+    if (arr[i].tag() == -1){
       return i;
     }
   }
   return -1;
+}
+void construct_tree_debug(int idx, int lower, int upper){
+  if (upper == lower){
+    return;
+  }
+  bits_debug[idx].upper = upper;
+  bits_debug[idx].lower = lower;
+  bits_debug[idx].hot_direction = "left";
+  cout<<"("<<lower<<", "<<upper<<") index: "<<idx<<endl;
+
+  //left call:
+  construct_tree_debug((idx *2) +1, lower, (lower+upper)/2);
+  //right call:
+  construct_tree_debug((idx *2) +2, ((lower+upper)/2)+1, upper);
+}
+void update_tree_debug(int input, int idx, int lower, int upper){
+  //idx will depend on value of input val...
+  if(upper == lower){
+    return;
+  }
+  //bits[idx].temperature = "hot";
+  if (input > (lower+upper)/2 && input <=upper){ // we will go to the right child
+    //set hot direction to right
+    bits_debug[idx].hot_direction = "right";
+    // go to right child
+    update_tree_debug(input,(2*idx)+2,((lower+upper)/2)+1, upper);
+  }
+  else{ // we will go to the left child
+    //set hot direction to left
+    bits_debug[idx].hot_direction = "left";
+    //go to left child
+    update_tree_debug(input,(idx *2) +1, lower, (lower+upper)/2);
+  }
+
 }
 void construct_tree(int idx, int lower, int upper){
   /*bits[idx].upper = upper;
@@ -103,7 +138,7 @@ void construct_tree(int idx, int lower, int upper){
   bits[idx].upper = upper;
   bits[idx].lower = lower;
   bits[idx].hot_direction = "left";
-  cout<<"("<<lower<<", "<<upper<<") index: "<<idx<<endl;
+  //cout<<"("<<lower<<", "<<upper<<") index: "<<idx<<endl;
 
   //left call:
   construct_tree((idx *2) +1, lower, (lower+upper)/2);
@@ -117,30 +152,45 @@ void update_tree(int input, int idx, int lower, int upper){
   }
   //bits[idx].temperature = "hot";
   if (input > (lower+upper)/2 && input <=upper){ // we will go to the right child
-    // first, left child set to cold
+    //set hot direction to right
     bits[idx].hot_direction = "right";
-    //now, jump to right child
+    // go to right child
     update_tree(input,(2*idx)+2,((lower+upper)/2)+1, upper);
   }
   else{ // we will go to the left child
-    //first, set right child to cold
+    //set hot direction to left
     bits[idx].hot_direction = "left";
-    //now, jump to right child
+    //go to left child
     update_tree(input,(idx *2) +1, lower, (lower+upper)/2);
   }
+
+}
+int fetch_debug(int idx, int lower, int upper){
+  if(upper == lower){
+    return lower;
+  }
+  if(bits_debug[idx].hot_direction == "left"){ // we will go to the right child, because the cold direction is right
+    //return fetch_index((idx *2)+1, lower, (lower+upper)/2);
+    return fetch_debug((2*idx)+2,((lower+upper)/2)+1, upper);
+  }
+
+    //return fetch_index((2*idx)+2,((lower+upper)/2)+1, upper);
+    return fetch_debug((idx *2)+1, lower, (lower+upper)/2);
+
 
 }
 int fetch_index(int idx, int lower, int upper){
   if(upper == lower){
     return lower;
   }
-  if(bits[idx].hot_direction == "right"){ // we will go to the left child, because the cold direction is left
-    return fetch_index((idx *2)+1, lower, (lower+upper)/2);
-  }
-  //we will go to the right child
-  else if(bits[idx].hot_direction == "left"){
+  if(bits[idx].hot_direction == "left"){ // we will go to the right child, because the cold direction is right
+    //return fetch_index((idx *2)+1, lower, (lower+upper)/2);
     return fetch_index((2*idx)+2,((lower+upper)/2)+1, upper);
   }
+
+    //return fetch_index((2*idx)+2,((lower+upper)/2)+1, upper);
+    return fetch_index((idx *2)+1, lower, (lower+upper)/2);
+
 
 }
 int main(int argc, char * argv[]){
@@ -156,11 +206,12 @@ int main(int argc, char * argv[]){
 
 
   //initializing bit tree
-  for(int i = 0;i<513;i++){
+  for(int i = 0;i<512;i++){
     Node my_node;
     //my_node.upper  = -1;
     //my_node.lower = -1;
     bits.push_back(my_node);
+    bits_debug.push_back(my_node);
   }
 
   // DEBUG: constructing real tree now:
@@ -268,7 +319,7 @@ result direct_map(int size){
   retval.accesses = 0;
   unsigned long long num_blocks = size/32;
   Line * cache =  new Line [num_blocks];
-  unsigned long long mask = num_blocks-1;
+  //unsigned long long mask = num_blocks-1;
   for(unsigned long long i=0; i<lines.size();i++){
     unsigned long long chopped = lines[i].address() >> 5;
     unsigned long long index = chopped % num_blocks;
@@ -295,7 +346,7 @@ result part2_3(int a){
 
   //num_sets = 512;
   Set * cache = new Set [cache_size];
-  for (int i = 0; i<cache_size;i++){
+  for (unsigned long long i = 0; i<cache_size;i++){
     cache[i] = Set(a);
   }
   for (unsigned long long i=0;i<lines.size();i++){
@@ -318,6 +369,13 @@ result part2_3(int a){
 result part3b(){
   //construct the tree
   construct_tree(0,0,511);
+  //construct_tree_debug(0,0,15);
+  //int test = fetch_debug(0,0,15);
+  //cout<<"selected index before update: "<<test<<endl;
+  //update_tree_debug(7,0,0,15);
+  //update_tree_debug(15,0,0,15);
+  //int test2 = fetch_debug(0,0,15);
+  //cout<<"selected index after update: "<<test2<<endl;
   //cout<<"tree constructed"<<endl;
   Line cache [512];
   bool hit;
@@ -325,7 +383,7 @@ result part3b(){
   //cout<<"pointer created"<<endl;
   for (int i = 0;i<512;i++){//create blank lines
     cache[i] = Line();
-    cache[i].setFlag("blank");
+    cache[i].setTag(-1);
     //cout<<"cache initialized"<<endl;
   }
 
@@ -333,40 +391,46 @@ result part3b(){
   retval.hits = 0;
   retval.accesses = 0;
   for (unsigned long long i = 0;i<lines.size();i++){
-    hit = false;
+    //hit = false;
     unsigned long long chopped = lines[i].address() >> 5;
-    unsigned long long index = chopped %512;
-    unsigned long long tag = chopped >> (unsigned long long)log2(512);
+    //unsigned long long index = chopped %512;
+    unsigned long long tag = chopped >> (unsigned long long)log2(1);
     lines[i].setTag(tag);
     for(int j = 0;j<512;j++){
-      if(lines[i].tag() == cache[j].tag()){
+      if(tag == cache[j].tag()){
         //retval.hits++;
-        //update_tree(j,0,0,511);
+        //cout<<"j: "<<j<<endl;
+        update_tree(j,0,0,511);
+
+        retval.hits++;
+
         hit = true;
         break;
       }
     }
     if (!hit){
       int check = free_spot(cache);
+      cout<<"check: "<<check<<endl;
       if (check!= -1){
         cache[check] = lines[i];
         update_tree(check,0,0,511);
       }
       else{
+        //cout<<"got to this point"<<endl;
         int hotCold = fetch_index(0,0,511);
+        //cout<<hotCold<<endl;
         cache[hotCold] = lines[i];
         update_tree(hotCold,0,0,511);
       }
+    }
 
-    }
-    else{
-      retval.hits++;
-    }
 
     retval.accesses++;
+    hit = false;
   }
 
   //delete [] cache;
+
   return retval;
 
 }
@@ -378,7 +442,7 @@ result part4(int a){
   unsigned long long set_size = 32 * a;
   unsigned long long cache_size = 16384/set_size;
   Set * cache = new Set [cache_size];
-  for(int i = 0;i<cache_size;i++){
+  for(unsigned long long i = 0;i<cache_size;i++){
     cache[i] = Set(a);
   }
   for(unsigned long long i = 0;i<lines.size();i++){
@@ -409,7 +473,7 @@ result part5(int a){
   retval.hits = 0;
   retval.accesses = 0;
   Set * cache = new Set [cache_size];
-  for (int i = 0; i<cache_size;i++){
+  for (unsigned long long i = 0; i<cache_size;i++){
     cache[i] = Set(a);
   }
   for (unsigned long long i=0;i<lines.size();i++){
@@ -443,7 +507,7 @@ result part6(int a){
   retval.hits = 0;
   retval.accesses = 0;
   Set * cache = new Set [cache_size];
-  for (int i = 0; i<cache_size;i++){
+  for (unsigned long long i = 0; i<cache_size;i++){
     cache[i] = Set(a);
   }
   for(unsigned long long i = 0;i<lines.size();i++){
